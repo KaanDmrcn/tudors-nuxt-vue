da0024ecb5202b552ca3d6891ce02c05741ab884
Kaan Demircan <kaandemircan97@gmail.com>
2021-10-31 22:26:33 +0300

    "Product detail table" component added

---
+++ components/ProductDetailTable.vue
@@ -0,0 +1,52 @@
+<template>
+  <table class="table table-striped table-bordered">
+
+  <tbody style="font-size:13px;">
+    <tr>
+      <th scope="row">ÜRÜN</th>
+      <td style="color:grey;">Kazak</td>
+
+    </tr>
+    <tr>
+      <th scope="row">KALIP</th>
+      <td style="color:grey;">Slim Fit - Dar Kesim</td>
+
+    </tr>
+    <tr>
+      <th scope="row">DESEN</th>
+      <td style="color:grey;">Armürlü</td>
+
+    </tr>
+        <tr>
+      <th scope="row">RENK</th>
+      <td style="color:grey;">Gri</td>
+
+    </tr>
+    <tr>
+      <th scope="row">YAKA TİPİ</th>
+      <td style="color:grey;">Bisiklet Yaka</td>
+
+    </tr>
+    <tr>
+      <th scope="row">KOL TİPİ</th>
+      <td style="color:grey;">Uzun Kol</td>
+
+    </tr>
+        <tr>
+      <th scope="row">MATERYAL</th>
+      <td style="color:grey;">Pamuk%50 Pamuk %50 Akrilik</td>
+
+    </tr>
+    <tr>
+      <th scope="row">STİL</th>
+      <td style="color:grey;">Günlük, Rahat Kullanım</td>
+
+    </tr>
+    <tr>
+      <th scope="row">SEZON</th>
+      <td style="color:grey;">Kışlık</td>
+
+    </tr>
+  </tbody>
+</table>
+</template>
